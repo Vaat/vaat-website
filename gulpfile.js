68ea88f5d7e4dda1e71af2b78570d4c6effc93f8
markushausammann <markus.hausammann@cloud-solutions.net>
2016-11-12 22:35:15 +0200

    first rough translation to bootstrap, introducing gulp, sass, npm

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', ['sass'], function () {
+
+});
+
+gulp.task('sass', function () {
+    return gulp.src('scss/styles.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'));
+});
