2fb58b09e854cfe70dca8609386887d402929906
markushausammann <markus.hausammann@brightanswer.ch>
2020-09-15 22:38:01 +0200

    re-layout according to Oliver

--- web/public/revolution.php
+++ web/public/revolution.php
@@ -9,73 +9,59 @@
 
     <!-- Primary Meta Tags -->
     <title>Vaat Brewery</title>
-    <meta name="title" content="Vaat Brewery">
+    <meta name="title" content="Vaat Revolution">
     <meta name="description" content="Join the next micro brewing revolution!">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://vaat.ee/revolution/">
-    <meta property="og:title" content="Vaat Brewery">
+    <meta property="og:title" content="Vaat Revolution">
     <meta property="og:description" content="Join the next micro brewing revolution!">
     <meta property="og:image" content="http://vaat.ee/images/opengraph-header-cells.png">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://vaat.ee/revolution/">
-    <meta property="twitter:title" content="Vaat Brewery">
+    <meta property="twitter:title" content="Vaat Revolution">
     <meta property="twitter:description" content="Join the next micro brewing revolution!">
     <meta property="twitter:image" content="http://vaat.ee/images/opengraph-header-cells.png">
 
     <link rel="stylesheet" type="text/css" href="css/revolution-styles.css" />
 </head>
 <body>
-<div class="container-fluid bg-light top-container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
-            <img class="top-row" src="images/logo-blue.svg" alt="VAAT" width="72"/>
-            <h1>
-                Join the next <br/>
-                micro brewing revolution!
-            </h1>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
-            <img class="pull-right" src="images/hand.svg" alt="V sign"/>
+        <div class="col-lg-12">
+            <img src="images/logo-blue.svg" class="logo-text" alt="VAAT" />
         </div>
     </div>
-</div>
-<div class="custom-shape-divider-bottom-1598558841 bg-light">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-    </svg>
-</div>
-<div class="container-fluid bg-dark bottom-container">
-    <div class="row top-row">
-        <div class="col-md-6">
-            <p class="vision">
-                We are building a modern production brewery and a tap room in Tallinn, Estonia!
-                We've raised about 70% of the funds so far.
-            </p>
-            <p class="vision">
-                <span class="text-important">But we need YOU</span> to get us over the finish line!
-            </p>
-        </div>
-        <div class="col-md-6">
-            <p class="vision">
-                Our <strong>VISION</strong> is a place of great fresh beers, low- and non-alcoholic alternatives
+    <div class="row">
+        <div class="col-md-6 col-sm-12 bg-light">
+            <h1 class="padding-right">
+                Join the next micro brewing revolution!
+            </h1>
+            <p class="padding-right">
+                Our VISION is a place of great fresh beers, low- and non-alcoholic alternatives
                 and other interesting drinks where beer lovers as well as brewers from around the world meet to
                 witness the latest innovation in ecologically sustainable, high-quality micro brewing.
             </p>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-            <p class="vision">Sign up for Crowd Funding news:</p>
-            <div id="mc_embed_signup">
+        <div class="col-md-6 col-sm-12 bg-dark">
+            <p class="padding-left">
+                We are building a modern production brewery and a tap room in Tallinn, Estonia!
+                We've raised about 70% of the funds so far.
+            </p>
+            <p class="padding-left">
+                But we need YOU to get us over the finish line!
+            </p>
+            <br/>
+            <p class="padding-left">Sign up for Crowd Funding news:</p>
+            <div id="mc_embed_signup" class="padding-left">
                 <form action="https://vaat.us18.list-manage.com/subscribe/post?u=16f8ddce67821eefb8462635f&amp;id=8dc4229418" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                     <div id="mc_embed_signup_scroll">
                         <div class="mc-field-group input">
-                            <input type="email" value="" name="EMAIL" class="required email button" id="mce-EMAIL" placeholder="YOUR@EMAIL.COM">
-                            <input type="submit" value="SIGN UP" name="subscribe" id="mc-embedded-subscribe" class="button">
+                            <input type="email" value="" name="EMAIL" class="required email button" id="mce-EMAIL" placeholder="your@email.com">
+                            <input type="submit" value="Sign up" name="subscribe" id="mc-embedded-subscribe" class="button">
                         </div>
                         <div id="mce-responses" class="clear">
                             <div class="response" id="mce-error-response" style="display:none"></div>
@@ -87,9 +73,6 @@
                     </div>
                 </form>
             </div>
-        </div>
-        <div class="col-md-6">
-            <img class="light-cells pull-right hidden-sm hidden-xs" src="images/cells-2.svg" />
         </div>
     </div>
 </div>
