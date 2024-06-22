9992e4abc16f0ccedaaed6da3ed8e1a56f52d377
markushausammann <markus.hausammann@brightanswer.ch>
2024-06-23 02:21:00 +0300

    make css building work

--- build/config.base.js
+++ build/config.base.js
@@ -4,14 +4,15 @@ const { SRC, DIST } = require('./paths')
 
 module.exports = {
     entry: {
-        scripts: path.resolve(SRC, 'js', 'index.js')
+        scripts: path.resolve(SRC, 'js', 'index.js'),
+        styles: path.resolve(SRC, 'scss', 'styles.scss')
     },
     output: {
         // Put all the bundled stuff in your dist folder
-        path: path.resolve(DIST, 'js'),
+        path: path.resolve(DIST),
 
         // Our single entry point from above will be named "scripts.js"
-        filename: '[name].js',
+        filename: 'js/[name].js',
     },
     module: {
         rules: [
@@ -23,14 +24,24 @@ module.exports = {
                     loader: 'expose-loader',
                     options: { exposes: ["$", "jQuery"]}
                     }]
-            },
-            {
+            }, {
                 // Exposes lightGallery for use outside Webpack build
                 test: require.resolve('lightgallery'),
                 use: [{
                     loader: 'expose-loader',
                     options: { exposes: "lightGallery"}
                     }]
+            }, {
+                // compile sass files
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: { outputPath: 'css/', name: '[name].min.css'}
+                    },
+                    'sass-loader'
+                ]
             }
         ]
     },
