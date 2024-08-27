09032ee4b5c4aff9961efd344be12390e99e61c9
markushausammann <markus.hausammann@brightanswer.ch>
2024-08-27 03:17:33 +0300

    make the file php for future automation, improve styling

---
+++ public/brewing.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Vaat Brewery">
+    <meta name="author" content="Vaat OÜ">
+    <link rel="icon" href="favicon.ico">
+    <title>Vaat</title>
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Inter">
+    <link rel="stylesheet" type="text/css" href="css/styles.min.css" />
+</head>
+
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="brewing">Vaat's brewing?</h2>
+                    <hr/>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2 col-sm-4 tank-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <img class="can" src="images/tank.png" alt="tank" />
+                        </div>
+                        <div class="col-lg-12">
+                            <h3>Tank 1</h3>
+                            <h4>empty</h4>
+                            <p class="beer-description">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-4 tank-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <img class="can" src="images/tank.png" alt="tank" />
+                        </div>
+                        <div class="col-lg-12">
+                            <h3>Tank 2</h3>
+                            <h4>Finally!</h4>
+                            <p class="beer-description">
+                                BD: 26.08.2024
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-4 tank-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <img class="can" src="images/tank.png" alt="tank" />
+                        </div>
+                        <div class="col-lg-12">
+                            <h3>Tank 3</h3>
+                            <h4>Fizzy Bee</h4>
+                            <p class="beer-description">
+                                BD: 12.06.2024
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-4 tank-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <img class="can" src="images/tank.png" alt="tank" />
+                        </div>
+                        <div class="col-lg-12">
+                            <h3>Tank 4</h3>
+                            <h4>empty</h4>
+                            <p class="beer-description">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-4 tank-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <img class="can" src="images/tank.png" alt="tank" />
+                        </div>
+                        <div class="col-lg-12">
+                            <h3>Tank 5</h3>
+                            <h4>empty</h4>
+                            <p class="beer-description">
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-sm-4 tank-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <img class="can" src="images/tank.png" alt="tank" />
+                        </div>
+                        <div class="col-lg-12">
+                            <h3>Tank 6</h3>
+                            <h4>People Pleasner</h4>
+                            <p class="beer-description">
+                                BD: 22.08.2024
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/scripts.js"></script>
+</body>
+</html>
